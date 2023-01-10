9734ac8257527917fc1c49a1afacfae8384acc29
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2023-01-10 17:48:54 -0300

    App Terminada

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './convertidor.component.html',
   styleUrls: ['./convertidor.component.css']
 })
+
 export class ConvertidorComponent {
 
   cantidad = 0;
@@ -13,5 +14,54 @@ export class ConvertidorComponent {
   total = 0;
 
   monedas: string[] = ['DOLAR', 'EURO', 'LIBRA']
+
+
+  convertir() {
+
+    switch(this.tengo){
+      case 'DOLAR':
+        if(this.quiero === 'DOLAR'){
+          this.total = this.cantidad;
+        }
+
+        if(this.quiero === 'EURO'){
+          this.total = this.cantidad * 0.84;
+        }
+
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.75;
+        }
+      break;
+
+      case 'EURO':
+        if(this.quiero === 'DOLAR'){
+          this.total = this.cantidad * 1.20;
+        }
+
+        if(this.quiero === 'EURO'){
+          this.total = this.cantidad;
+        }
+
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.90;
+        }
+      break;
+
+      case 'LIBRA':
+        if(this.quiero === 'DOLAR'){
+          this.total = this.cantidad * 1.33;
+        }
+
+        if(this.quiero === 'EURO'){
+          this.total = this.cantidad * 1.11;
+        }
+
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad;
+        }
+      break;
+
+    }
+  }
 
 }
